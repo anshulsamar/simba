4a989f63d0550e0ab810710d78401947d0073aff
Anshul Samar <asamar@stanford.edu>
2013-08-23 10:56:36 -0700

    added library path change script

---
+++ changeLibraryPaths.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+make clean
+make
+rm -rf TLD.app/Contents/Frameworks/*
+cp /usr/X11/lib/libpng15.15.dylib TLD.app/Contents/Frameworks/
+install_name_tool -change /usr/X11/lib/libpng15.15.dylib @executable_path/../Frameworks/libpng15.15.dylib TLD.app/Contents/MacOS/tld
+install_name_tool -id @executable_path/../Frameworks/libpng15.15.dylib TLD.app/Contents/Frameworks/libpng15.15.dylib
+install_name_tool -change /usr/local/lib/liblinear.dylib @executable_path/../Frameworks/liblinear.dylib TLD.app/Contents/MacOS/tld
+
+exit
+
